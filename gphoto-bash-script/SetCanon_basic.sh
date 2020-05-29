782835d4ecb11e9c8d1fbf9385cf6823dc563533
jpl <jpl@zju.edu.cn>
2020-05-29 15:51:43 +0800

    update install scripts

--- gphoto-bash-script/SetCanon_basic.sh
+++ gphoto-bash-script/SetCanon_basic.sh
@@ -9,7 +9,7 @@ for portname in $(gphoto2 --auto-detect | grep "usb" | sed 's/^.*usb://g');do
 	#echo 'Port: '$portname
 
 	gphoto2 --port "usb:$portname"                  \
-	--set-config          shutterspeed="1/50"      \
+	--set-config          shutterspeed="1/100"      \
 	--set-config          aperture=16   	        \
 	--set-config          iso=100                   \
 	--set-config-index    imageformat=1             \
